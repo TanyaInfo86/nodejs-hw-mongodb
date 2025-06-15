8986497bd86b22fe1342ee58a83f5a02b9032feb
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-06-15 19:15:51 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
-    await initMongoConnection();
-    setupServer();
+    try {
+        await initMongoConnection();
+        setupServer();
+    } catch (error) {
+        console.error('Failed to start app:', error);
+        process.exit(1);
+    }
 };
 
 bootstrap();
