ca16dea3a64166b7a550b365cfb4b670ab17c4fd
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-07-06 00:49:27 +0300

    add index

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,6 @@ export const SORT_ORDER = {
     ASC: 'asc',
     DESC: 'desc',
 };
-
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const SMTP = {
@@ -13,11 +12,9 @@ export const SMTP = {
     SMTP_PASSWORD: 'SMTP_PASSWORD',
     SMTP_FROM: 'SMTP_FROM',
 };
-
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
-
 export const CLOUDINARY = {
     CLOUD_NAME: 'CLOUD_NAME',
     API_KEY: 'API_KEY',
