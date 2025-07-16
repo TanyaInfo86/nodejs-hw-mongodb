f5bd8bac3f80b0d17edde118e7f4419e64887f63
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-07-16 16:00:10 +0300

    WIP: збереження змін перед переходом на hw6-email-and-images

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,33 +1,13 @@
-// import swaggerUI from 'swagger-ui-express';
-// import fs from 'node:fs';
-// import { SWAGGER_PATH } from '../constants/index.js';
-// import createHttpError from 'http-errors';
-
-
-// export const swaggerDocs = () => {
-//   try {
-//     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-//     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-//   } catch {
-//     return (req, res, next) =>
-//       next(createHttpError(500, "Can't load swagger docs"));
-//   }
-// };
 import swaggerUI from 'swagger-ui-express';
-import fs from 'node:fs';
-import { SWAGGER_PATH } from '../constants/index.js';
+import YAML from 'yamljs';
 import createHttpError from 'http-errors';
 
-let swaggerMiddleware;
-
-try {
-  const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf-8'));
-  swaggerMiddleware = [swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-} catch (error) {
-  console.error('❌ Failed to load Swagger docs:', error.message);
-  swaggerMiddleware = [
-    (req, res, next) => next(createHttpError(500, "Can't load Swagger docs")),
-  ];
-}
-
-export const swaggerDocs = () => swaggerMiddleware;
+export const swaggerDocs = () => {
+  try {
+    const swaggerDocument = YAML.load('./docs/openapi.yaml');
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDocument)];
+  } catch (error) {
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load Swagger YAML documentation"));
+  }
+};
