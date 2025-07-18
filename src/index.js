9a332e2134e60a83b269ecddfe23ecf8378e6048
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-07-18 14:49:11 +0300

    fix ENABLE_CLOUDINARY

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+// import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
+// import { initMongoConnection } from './db/initMongoConnection.js';
+// import { setupServer } from './server.js';
+// import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+// import { getEnvVar } from './utils/getEnvVar.js';
+
+// console.log('CLOUD_NAME =', getEnvVar('CLOUD_NAME'));
+
+// const bootstrap = async () => {
+//   await initMongoConnection();
+//   await createDirIfNotExists(TEMP_UPLOAD_DIR);
+//   await createDirIfNotExists(UPLOAD_DIR);
+//   setupServer();
+// };
+// console.log('SMTP_USER:', process.env.SMTP_USER);
+// console.log('SMTP_PASSWORD:', process.env.SMTP_PASSWORD ? 'OK' : 'EMPTY or incorrect');
+// void bootstrap();
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
@@ -9,6 +26,5 @@ const bootstrap = async () => {
   await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
-console.log('SMTP_USER:', process.env.SMTP_USER);
-console.log('SMTP_PASSWORD:', process.env.SMTP_PASSWORD ? 'OK' : 'EMPTY or incorrect');
+
 void bootstrap();
