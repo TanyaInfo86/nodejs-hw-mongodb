589fa81f3cbfa5176dfada35f3b26c072db578db
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-07-19 09:18:28 +0300

    12

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,13 +1,16 @@
 import swaggerUI from 'swagger-ui-express';
-import YAML from 'yamljs';
+import fs from 'node:fs';
+import { SWAGGER_PATH } from '../constants/index.js';
 import createHttpError from 'http-errors';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDocument = YAML.load('./docs/openapi.yaml');
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDocument)];
+    const raw = fs.readFileSync(SWAGGER_PATH, 'utf-8');
+    const swaggerDoc = JSON.parse(raw);
+    return [swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (error) {
+    console.error('Failed to load Swagger docs:', error.message);
     return (req, res, next) =>
-      next(createHttpError(500, "Can't load Swagger YAML documentation"));
+      next(createHttpError(500, "Can't load swagger docs"));
   }
 };
